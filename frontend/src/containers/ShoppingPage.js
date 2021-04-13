e16320c183df5738a0a8044e5066ee0bb020b637
ferolic <ashwinferolic96@gmail.com>
2021-04-13 15:54:22 +0530

    Shopping Page styled, need to do product page

--- frontend/src/containers/ShoppingPage.js
+++ frontend/src/containers/ShoppingPage.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import Navbar from '../components/Navbar';
 import Product from '../components/Product';
 import Footer from '../components/Footer';
+import Button from '../components/Button';
 
 const Shop = styled.div`
   display: flex;
@@ -38,6 +40,75 @@ const ProductWrapper = styled.div`
   justify-content: space-between;
 `;
 
+const FirstProductWrapper = styled.div`
+  position: relative;
+  margin-bottom: 30px;
+  flex: 1 1 100%;
+  min-width: 300px;
+
+  @media ${(props) => props.theme.mediaQueries.large} {
+    flex: 0 0 100%;
+    margin-bottom: 64px;
+    -webkit-box-pack: justify;
+    justify-content: space-between;
+    display: flex;
+    -webkit-box-align: center;
+    align-items: center;
+    width: 48%;
+    min-width: unset;
+  }
+`;
+
+const FirstProductImageWrapper = styled.div`
+  position: relative;
+  width: 100%;
+
+  @media ${(props) => props.theme.mediaQueries.large} {
+    width: 48%;
+    margin: 0;
+  }
+`;
+
+const FirstProductImage = styled.img`
+  width: 100%;
+  border-radius: 4px;
+`;
+
+const FirstProductInfoWrapper = styled.div`
+  @media ${(props) => props.theme.mediaQueries.large} {
+    width: 45%;
+  }
+`;
+
+const FirstProductTitle = styled.h2`
+  font-weight: 800;
+  font-size: 24px;
+  line-height: 24px;
+  color: black;
+  margin: 20px 0 0;
+
+  @media ${(props) => props.theme.mediaQueries.large} {
+    font-size: 48px;
+    margin-top: 0;
+  }
+`;
+
+const FirstProductPrice = styled.p`
+  font-size: 20px;
+  line-height: 32px;
+  color: rgba(0, 0, 0, 0.45);
+  margin: 30px 0px;
+`;
+
+const FirstProductBtnWrapper = styled.div`
+  position: relative;
+  margin-top: 20px;
+
+  @media ${(props) => props.theme.mediaQueries.medium} {
+    display: none;
+  }
+`;
+
 const ShoppingPage = () => {
   return (
     <Shop>
@@ -46,6 +117,35 @@ const ShoppingPage = () => {
       </NavbarWrapper>
       <Wrapper>
         <ProductWrapper>
+          <FirstProductWrapper>
+            <FirstProductImageWrapper>
+              <Link to="/product/1">
+                <FirstProductImage src="../img/kit.webp" />
+              </Link>
+            </FirstProductImageWrapper>
+            <FirstProductInfoWrapper>
+              <Link to="/product/1a">
+                <FirstProductTitle> Home roasting kit </FirstProductTitle>
+              </Link>
+              <FirstProductPrice> € 99.00 </FirstProductPrice>
+              <FirstProductBtnWrapper>
+                <Button text="Add To Cart" />
+                <Button text="Buy Now" dark />
+              </FirstProductBtnWrapper>
+            </FirstProductInfoWrapper>
+          </FirstProductWrapper>
+
+          <div className="w-48">
+            <Product
+              id="1"
+              title="Kenya (300g)"
+              alt="Kenya (300g) product shot"
+              src="../img/kenya.webp"
+              price="7.50"
+              showPopup
+            />
+          </div>
+
           <div className="w-48">
             <Product
               id="2"
