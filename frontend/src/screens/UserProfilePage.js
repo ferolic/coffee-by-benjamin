98d5433aa0c60eb9ac2ade3b6f0d9bbd673049b3
ferolic <ashwinferolic96@gmail.com>
2021-05-16 14:49:18 +0530

    get orders in profile done , need to style profile page

--- frontend/src/screens/UserProfilePage.js
+++ frontend/src/screens/UserProfilePage.js
@@ -1,7 +1,47 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { listMyOrders } from '../actions/orderActions';
 
-const UserProfilePage = () => {
-  return <div>Profile</div>;
+const UserProfilePage = ({ history }) => {
+  const orderListMy = useSelector((state) => state.orderListMy);
+  const { loading: loadingOrders, orders, error: errorOrders } = orderListMy;
+
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!userInfo) {
+      history.push('/login');
+    } else {
+      dispatch(listMyOrders());
+    }
+  }, [userInfo, dispatch, history]);
+
+  return (
+    <>
+      {loadingOrders ? (
+        <p> Loading... </p>
+      ) : errorOrders ? (
+        <p> {errorOrders} </p>
+      ) : (
+        <div>
+          {orders.length === 0 ? (
+            <p> You don't have any orders </p>
+          ) : (
+            <>
+              {orders.map((order) => (
+                <div key={order._id}>
+                  <p> {order._id} </p>
+                </div>
+              ))}
+            </>
+          )}
+        </div>
+      )}
+    </>
+  );
 };
 
 export default UserProfilePage;
